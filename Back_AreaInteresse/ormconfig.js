ac3975f9a21e379ffc7e60eeb382886a780e21d3
Grazi105 <100172450+Grazi105@users.noreply.github.com>
2023-10-24 19:44:22 -0300

    Add files via upload

---
+++ Back_AreaInteresse/ormconfig.js
@@ -0,0 +1,13 @@
+//ormconfig.js
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432, 
+  username: 'root',
+  password: '12343421',
+  database: 'UX',
+  synchronize: true, 
+  entities: [
+    'src/models/AreaInteresse.js' 
+  ],
+};
